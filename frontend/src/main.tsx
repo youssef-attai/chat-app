ae9adc046922966790d53a85469d502aeb1ed3ea
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-09 22:57:57 +0200

    Use the authentication provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import AuthProvider from './providers/AuthProvider'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <AuthProvider>
     <App />
+  </AuthProvider>,
 )
