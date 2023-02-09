412a4575fcb53d4e5f258a6b82a937396d867b6d
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-09 23:03:22 +0200

    Add socket.io to chat context provider

--- frontend/src/providers/ChatProvider.tsx
+++ frontend/src/providers/ChatProvider.tsx
@@ -1,9 +1,13 @@
 import { PropsWithChildren, useEffect, useState } from "react";
+import io, { Socket } from "socket.io-client";
 import ChatContext, { ChatContextValue } from "../contexts/ChatContext";
 import useAuth from "../hooks/useAuth";
 
+let socket: Socket;
+
 export function ChatProvider({ children }: PropsWithChildren) {
     const { currentUser } = useAuth()
+    const [receiveChatMsgCallbacks, setReceiveChatMsgCallbacks] = useState<Function[]>([])
 
     useEffect(() => {
         if (currentUser === undefined) { } else socket = io('http://localhost:3000', { query: { userId: currentUser.userId } })
@@ -13,6 +17,25 @@ export function ChatProvider({ children }: PropsWithChildren) {
         }
     }, [currentUser])
 
+    useEffect(() => {
+        socket.on('receive-chat-msg', (message) => {
+            receiveChatMsgCallbacks.forEach(cb => cb(message))
+        })
+        
+        return () => {
+            socket.off('receive-chat-msg')
+        }
+    }, [receiveChatMsgCallbacks])
+
+    const sendMessage = async (message: string, roomId: string) => {
+        socket.emit('send-chat-msg', {
+            content: message, roomId, sender: currentUser?.userId
+        })
+    }
+
+    const onReceiveMsg = (callback: Function) => {
+        setReceiveChatMsgCallbacks(prev => [...prev, callback])
+    }
 
     const contextValue: ChatContextValue = { sendMessage, onReceiveMsg }
 
