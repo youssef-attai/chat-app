1b68ee0714fdfd3f01c61eee3b00a669517550e2
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-09 23:00:57 +0200

    Conditionally render pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,2 +1,9 @@
+import useAuth from "./hooks/useAuth"
+import AuthenticationPage from "./pages/authentication/page"
+import MainPage from "./pages/main/page"
+import { ChatProvider } from "./providers/ChatProvider"
+
 export default function App() {
+  const { currentUser } = useAuth()
+  return currentUser === undefined ? <AuthenticationPage /> : <ChatProvider><MainPage /></ChatProvider>
 }
