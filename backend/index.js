90db55b2e9fd375a331eb5cafaabf2df859896d9
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-09 22:50:42 +0200

    Create a fake database

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+
+let latestMsgId = 3
+
+const users = [
+    {
+        userId: 'youssef',
+        username: 'Youssef Attai',
+        rooms: ['potato', 'asgard']
+    },
+    {
+        userId: 'omar',
+        username: 'Omar Taai',
+        rooms: ['potato']
+    },
+    {
+        userId: 'ashraf',
+        username: 'AboAshraf',
+        rooms: ['asgard']
+    }
+]
+
+const rooms = [
+    {
+        roomId: 'potato',
+        roomName: 'ThePotatoes',
+        messages: [
+            {
+                messageId: 'msg1',
+                sender: 'youssef',
+                content: 'lol'
+            },
+            {
+                messageId: 'msg2',
+                sender: 'omar',
+                content: 'damn'
+            }
+        ]
+    },
+    {
+        roomId: 'asgard',
+        roomName: 'Asgardians of the GALAxi',
+        messages: [
+            {
+                messageId: 'msg3',
+                sender: 'ashraf',
+                content: 'ya sater'
+            }
+        ]
+    },
+]
+
