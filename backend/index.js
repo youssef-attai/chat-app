8dfbfe8dd7c9f49b06807816406c43239bf261e2
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-10 13:10:11 +0200

    Move endpoints to their own router

--- backend/index.js
+++ backend/index.js
@@ -1,62 +1,24 @@
-import express from 'express'
-import cors from 'cors'
-import { Server } from 'socket.io'
-import http from 'http'
-
-let latestMsgId = 3
-
-const users = [
-    {
-        userId: 'youssef',
-        username: 'Youssef Attai',
-        rooms: ['potato', 'asgard']
-    },
-    {
-        userId: 'omar',
-        username: 'Omar Taai',
-        rooms: ['potato']
-    },
-    {
-        userId: 'ashraf',
-        username: 'AboAshraf',
-        rooms: ['asgard']
-    }
-]
-
-const rooms = [
-    {
-        roomId: 'potato',
-        roomName: 'ThePotatoes',
-        messages: [
-            {
-                messageId: 'msg1',
-                sender: 'youssef',
-                content: 'lol'
-            },
-            {
-                messageId: 'msg2',
-                sender: 'omar',
-                content: 'damn'
-            }
-        ]
-    },
-    {
-        roomId: 'asgard',
-        roomName: 'Asgardians of the GALAxi',
-        messages: [
-            {
-                messageId: 'msg3',
-                sender: 'ashraf',
-                content: 'ya sater'
-            }
-        ]
-    },
-]
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { Server } from 'socket.io';
+import http from 'http';
+import authRouter from './routers/auth.js';
+import roomsRouter from './routers/room.js';
+import friendsRouter from './routers/friends.js';
+import './database/database.js';
+import { PORT } from './env.js';
+import User from './models/User.js';
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
+
+app.use(cookieParser());
+app.use(express.json());
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -65,27 +27,21 @@ const io = new Server(server, {
     }
 });
 
-app.post('/login', (req, res) => {
-    const userId = req.body.userId
-    res.json({ user: users.find(u => u.userId === userId) })
-})
+app.use('/auth', authRouter);
+app.use('/rooms', roomsRouter);
+app.use('/friends', friendsRouter);
 
-app.get('/:userId/rooms', (req, res) => {
-    const userId = req.params.userId;
-    res.json({ rooms: users.find(u => u.userId === userId).rooms.map(roomId => rooms.find(r => r.roomId === roomId)) })
-});
-
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     const userId = socket.handshake.query.userId
 
     console.log(userId, 'connected');
 
-    users.find(u => u.userId === userId).rooms.forEach(r => socket.join(r))
+    const foundUser = await User.findById(userId);
+    foundUser.rooms.forEach(r => socket.join(r))
 
     socket.on('send-chat-msg', (message) => {
         console.log(message);
-        latestMsgId++
-        io.in(message.roomId).emit('receive-chat-msg', {...message, messageId: `msg${latestMsgId}`})
+        io.in(message.roomId).emit('receive-chat-msg', { ...message, messageId: `msg${latestMsgId}` })
     })
 
     socket.on('disconnect', () => {
@@ -93,7 +49,7 @@ io.on('connection', (socket) => {
     })
 });
 
-server.listen(3000, () => {
-    console.log(`server running on http://localhost:${3000}`);
+server.listen(PORT, () => {
+    console.log(`server running on http://localhost:${PORT}`);
 });
 
