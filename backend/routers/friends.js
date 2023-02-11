26c556da24ba24c6a7d5248bf3c9d167470f0379
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-11 13:13:00 +0200

    Create endpoints to get and add friends

---
+++ backend/routers/friends.js
@@ -0,0 +1,38 @@
+import User from "../models/User.js";
+import { Router } from "express";
+
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+
+const router = Router();
+
+function getFriendsResponse(friends) {
+    return friends.map(friend => (
+        {
+            userId: friend._id.toString(),
+            username: friend.username
+        }
+    ));
+}
+
+router.get('/', isAuthenticated, async (req, res) => {
+    try {
+        const friends = await User.find({ _id: { $in: req.currentUser.friends } });
+        res.status(200).json(getFriendsResponse(friends));
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'something went wrong' });
+    }
+});
+
+router.post('/request', isAuthenticated, async (req, res) => {
+    try {
+        const friend = await User.findOne({ username: req.body.username });
+        await req.currentUser.updateOne({ $push: { friends: friend._id } });
+        await User.findOneAndUpdate({ _id: friend._id }, { $push: { friends: req.currentUser._id } });
+        res.status(200).json({ message: `you are new friends with "${req.body.username}"` })
+    } catch (error) {
+        res.status(400).json({ message: `invalid username: "${req.body.username}"` });
+    }
+});
+
+export default router;
