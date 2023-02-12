ce605d35a79edcf7d37d30f68eca8fb232590b00
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-12 13:16:27 +0200

    Take signup input from user

--- frontend/src/pages/authentication/signup/page.tsx
+++ frontend/src/pages/authentication/signup/page.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+import useAuth from "../../../hooks/useAuth";
 import { useAuthenticationPage } from "../hook"
 
 function SignUpPage() {
     const { navigate } = useAuthenticationPage()
+    const { signUp } = useAuth()
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <>
             <h1>Sign up</h1>
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="username" />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="password" />
+            <button onClick={() => { signUp(username, password) }}>sign up</button>
             <button onClick={() => { navigate('login') }}>go to login page</button>
         </>
     )
