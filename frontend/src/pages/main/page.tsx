6364e6053f06a65cc5e6f6e642c92e411637debd
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-12 13:17:58 +0200

    Add the room creation and friend request pages

--- frontend/src/pages/main/page.tsx
+++ frontend/src/pages/main/page.tsx
@@ -1,11 +1,32 @@
 import ChatPage from "./chat/page"
+import CreateRoomPage from "./createRoom/page"
+import FriendRequestPage from "./friendRequest/page"
 import useMainPage from "./hook"
 import MainPageProvider from "./provider"
 import SettingsPage from "./settings/page"
 
 function Page() {
     const { currentPage } = useMainPage()
-    return currentPage === 'chat' ? <ChatPage /> : <SettingsPage />
+    let page;
+    switch (currentPage) {
+        case "chat": {
+            page = <ChatPage />;
+            break;
+        }
+        case "settings": {
+            page = <SettingsPage />
+            break;
+        }
+        case "createRoom": {
+            page = <CreateRoomPage />
+            break;
+        }
+        case "friendRequest": {
+            page = <FriendRequestPage />
+            break;
+        }
+    }
+    return page;
 }
 
 export default function MainPage() {
