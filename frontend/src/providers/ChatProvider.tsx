01a6ade405d467f028664abb13717139d78a1e78
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-12 13:18:11 +0200

    Reformat

--- frontend/src/providers/ChatProvider.tsx
+++ frontend/src/providers/ChatProvider.tsx
@@ -6,22 +6,22 @@ import useAuth from "../hooks/useAuth";
 let socket: Socket;
 
 export function ChatProvider({ children }: PropsWithChildren) {
-    const { currentUser } = useAuth()
-    const [receiveChatMsgCallbacks, setReceiveChatMsgCallbacks] = useState<Function[]>([])
+    const { currentUser } = useAuth();
+    const [receiveChatMsgCallbacks, setReceiveChatMsgCallbacks] = useState<Function[]>([]);
 
     useEffect(() => {
-        if (currentUser === undefined) { } else socket = io('http://localhost:3000', { query: { userId: currentUser.userId } })
+        if (currentUser === undefined) { } else socket = io('http://localhost:3000', { query: { userId: currentUser.userId } });
 
         return () => {
-            socket.disconnect()
+            socket.disconnect();
         }
-    }, [currentUser])
+    }, [currentUser]);
 
     useEffect(() => {
         socket.on('receive-chat-msg', (message) => {
             receiveChatMsgCallbacks.forEach(cb => cb(message))
         })
-        
+
         return () => {
             socket.off('receive-chat-msg')
         }
@@ -29,7 +29,7 @@ export function ChatProvider({ children }: PropsWithChildren) {
 
     const sendMessage = async (message: string, roomId: string) => {
         socket.emit('send-chat-msg', {
-            content: message, roomId, sender: currentUser?.userId
+            content: message, roomId, sender: currentUser!.userId
         })
     }
 
@@ -39,7 +39,9 @@ export function ChatProvider({ children }: PropsWithChildren) {
 
     const contextValue: ChatContextValue = { sendMessage, onReceiveMsg }
 
-    return <ChatContext.Provider value={contextValue}>
-        {children}
-    </ChatContext.Provider>
+    return (
+        <ChatContext.Provider value={contextValue}>
+            {children}
+        </ChatContext.Provider>
+    )
 }
