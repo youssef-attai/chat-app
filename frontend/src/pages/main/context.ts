37d197c1e0354e16ff9027b944cc3d807194e124
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-12 13:17:38 +0200

    Add the room creation and friend request pages

--- frontend/src/pages/main/context.ts
+++ frontend/src/pages/main/context.ts
@@ -1,12 +1,12 @@
 import { createContext } from "react"
 
-export type pages = 'chat' | 'settings'
+export type pages = 'chat' | 'settings' | 'createRoom' | 'friendRequest';
 
 export type MainPageContextValue = {
     currentPage: pages
     navigate: React.Dispatch<React.SetStateAction<pages>>
-}
+};
 
-const MainPageContext = createContext({} as MainPageContextValue)
+const MainPageContext = createContext({} as MainPageContextValue);
 
-export default MainPageContext
+export default MainPageContext;
